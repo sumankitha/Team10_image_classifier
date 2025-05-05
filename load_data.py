cbdd56bbae8142e9977249683ff806376f97f984
sumankitha <d.sumankitha@s.amity.edu>
2025-05-05 21:49:32 +0530

    code

--- load_data.py
+++ load_data.py
@@ -1,6 +1,11 @@
 the image classifier
 
-the def load_data(train_dir,val_dir,
+the introduction to the code
+
+
+
+
+def load_data(train_dir,val_dir,
 test_dir,img_size=IMG_SIZE,
               batch_size=BATCH_SIZE):
     datagen =
