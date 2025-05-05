f9e9f2e4e648b107f431987b61c8f72496d279f0
sumankitha <d.sumankitha@s.amity.edu>
2025-05-05 21:14:08 +0530

    data

--- load_data.py
+++ load_data.py
@@ -2,4 +2,7 @@ def load_data(train_dir,val_dir,
 test_dir,img_size=IMG_SIZE,
               batch_size=BATCH_SIZE):
     datagen =
-    ImageDataGeneration(rescale=1
+    ImageDataGeneration(rescale=1./255)
+
+    train_data=
+    datagen.flow_from_directory(
