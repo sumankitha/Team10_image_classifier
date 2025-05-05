f214a7d59e5a123fe1b03fcd160a00003f3a81b5
sumankitha <d.sumankitha@s.amity.edu>
2025-05-05 21:23:33 +0530

    val

--- load_data.py
+++ load_data.py
@@ -8,3 +8,7 @@ test_dir,img_size=IMG_SIZE,
     datagen.flow_from_directory(
             train_dir,target_size=img_size,
             batch_size=batch_size,class_mode='categorical'
+
+            val_data=
+            datagen.flow_from_directory(
+                val_dir,target
